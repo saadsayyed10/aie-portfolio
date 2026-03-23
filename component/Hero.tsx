1b3443bb6700da42911b4937cd0432d4a77c51dc
Saad Sayyed <saadsyed950@gmail.com>
2026-03-23 18:23:40 +0530

    Project Cards Layout

--- component/Hero.tsx
+++ component/Hero.tsx
@@ -1,17 +1,17 @@
 const Hero = () => {
   return (
-    <div className="flex justify-start items-start w-full flex-col lg:gap-y-6">
+    <div className="flex justify-start items-start w-full flex-col lg:gap-y-2">
       <h5 className="lg:text-lg font-semibold uppercase tracking-wide">
         Full-Stack Engineer & AI Specialist
       </h5>
       <h1 className="lg:text-7xl w-[60%] font-bold capitalize">
         AI Engineering & Full-Stack Solutions.
       </h1>
-      <p className="lg:text-base font-medium w-[60%]">
+      <p className="lg:text-base font-medium w-[60%] lg:mt-4">
         Building intelligent agents and SaaS platforms that solve real-world
         problems. Synthesizing advanced logic with ethereal user experiences.
       </p>
-      <div className="flex justify-start items-start w-full lg:gap-x-4">
+      <div className="flex justify-start items-start w-full lg:gap-x-4 lg:mt-4">
         <button className="border">View My Work</button>
         <button className="bg-black text-white">Contact Me</button>
       </div>
--- component/Projects.tsx
+++ component/Projects.tsx
@@ -1,16 +1,22 @@
 const Projects = () => {
   return (
-    <div className="flex justify-between items-center w-full">
-      <div className="flex justify-start items-start w-full flex-col lg:gap-y-2">
-        <h6 className="lg:text-lg font-bold uppercase tracking-wide">
-          Feature Works
-        </h6>
-        <h2 className="lg:text-5xl font-bold">The Lab Records</h2>
+    <div className="flex justify-start items-start flex-col w-full lg:gap-y-6">
+      <div className="flex justify-between items-center w-full">
+        <div className="flex justify-start items-start w-full flex-col lg:gap-y-2">
+          <h6 className="lg:text-lg font-bold uppercase tracking-wide">
+            Feature Works
+          </h6>
+          <h2 className="lg:text-5xl font-bold">The Lab Records</h2>
+        </div>
+        <p className="text-left font-medium">
+          A curation of engineered systems, from cognitive focus aids to
+          enterprise-scale commerce architectures.
+        </p>
       </div>
-      <p className="text-left">
-        A curation of engineered systems, from cognitive focus aids to
-        enterprise-scale commerce architectures.
-      </p>
+      <div className="flex justify-center items-center w-full lg:gap-x-6">
+        <div className="h-100 border rounded-lg w-[50%]"></div>
+        <div className="h-100 border rounded-lg w-[50%]"></div>
+      </div>
     </div>
   );
 };
