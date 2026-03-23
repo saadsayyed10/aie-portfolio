210555ee073bab3ed5143ca2b8e8f1989f5dd023
Saad Sayyed <saadsyed950@gmail.com>
2026-03-23 18:17:02 +0530

    Project Layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/component/Hero";
+import Projects from "@/component/Projects";
 
 const Home = () => {
   return (
-    <div className="flex justify-center items-center w-full min-h-screen flex-col lg:gap-y-10 lg:px-8">
+    <div className="flex justify-center items-center w-full min-h-screen flex-col lg:gap-y-40 lg:px-8 lg:py-4">
       <Hero />
+      <Projects />
     </div>
   );
 };
---
+++ component/Projects.tsx
@@ -0,0 +1,18 @@
+const Projects = () => {
+  return (
+    <div className="flex justify-between items-center w-full">
+      <div className="flex justify-start items-start w-full flex-col lg:gap-y-2">
+        <h6 className="lg:text-lg font-bold uppercase tracking-wide">
+          Feature Works
+        </h6>
+        <h2 className="lg:text-5xl font-bold">The Lab Records</h2>
+      </div>
+      <p className="text-left">
+        A curation of engineered systems, from cognitive focus aids to
+        enterprise-scale commerce architectures.
+      </p>
+    </div>
+  );
+};
+
+export default Projects;
