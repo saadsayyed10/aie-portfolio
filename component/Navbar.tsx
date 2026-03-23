e616e5ef211182739f23c7711a9fedad03cf2187
Saad Sayyed <saadsyed950@gmail.com>
2026-03-23 17:55:32 +0530

    Navbar layout

---
+++ component/Navbar.tsx
@@ -0,0 +1,10 @@
+const Navbar = () => {
+  return (
+    <div className="flex justify-between items-center w-full lg:px-10 lg:py-6 bg-black text-white">
+      <h3>Saad Sayyed</h3>
+      <button>Resume</button>
+    </div>
+  );
+};
+
+export default Navbar;
