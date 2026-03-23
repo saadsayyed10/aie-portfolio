945915fddab946da70f4dfcfab5c02acb7c77231
Saad Sayyed <saadsyed950@gmail.com>
2026-03-23 18:07:18 +0530

    Hero Layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import Hero from "@/component/Hero";
+
 const Home = () => {
   return (
-    <div className="flex justify-center items-center w-full min-h-screen">
-      Home
+    <div className="flex justify-center items-center w-full min-h-screen flex-col lg:gap-y-10 lg:px-8">
+      <Hero />
     </div>
   );
 };
---
+++ component/Hero.tsx
@@ -0,0 +1,22 @@
+const Hero = () => {
+  return (
+    <div className="flex justify-start items-start w-full flex-col lg:gap-y-6">
+      <h5 className="lg:text-lg font-semibold uppercase tracking-wide">
+        Full-Stack Engineer & AI Specialist
+      </h5>
+      <h1 className="lg:text-7xl w-[60%] font-bold capitalize">
+        AI Engineering & Full-Stack Solutions.
+      </h1>
+      <p className="lg:text-base font-medium w-[60%]">
+        Building intelligent agents and SaaS platforms that solve real-world
+        problems. Synthesizing advanced logic with ethereal user experiences.
+      </p>
+      <div className="flex justify-start items-start w-full lg:gap-x-4">
+        <button className="border">View My Work</button>
+        <button className="bg-black text-white">Contact Me</button>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
