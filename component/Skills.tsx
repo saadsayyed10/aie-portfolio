9675315497ce84965207383bdaa1efb4139b5ea2
Saad Sayyed <saadsyed950@gmail.com>
2026-03-24 14:58:05 +0530

    Skills Styled

--- component/Skills.tsx
+++ component/Skills.tsx
@@ -1,17 +1,19 @@
 const Skills = () => {
   return (
-    <div className="flex justify-center items-center w-full flex-col lg:gap-y-2">
-      <h6 className="lg:text-lg font-semibold uppercase tracking-wide">
+    <div className="flex justify-center items-center w-full flex-col lg:gap-y-2 bg-black">
+      <h6 className="lg:text-lg font-semibold uppercase tracking-wide text-green-500">
         The Arsenal
       </h6>
-      <h2 className="lg:text-5xl font-bold">Core Technologies</h2>
+      <h2 className="lg:text-5xl text-white font-bold">
+        Core Technologies<span className="text-green-500">.</span>
+      </h2>
       <div className="flex justify-center items-center w-full lg:gap-x-10 lg:mt-8">
-        <div className="border w-24 h-24 rounded-lg"></div>
-        <div className="border w-24 h-24 rounded-lg"></div>
-        <div className="border w-24 h-24 rounded-lg"></div>
-        <div className="border w-24 h-24 rounded-lg"></div>
-        <div className="border w-24 h-24 rounded-lg"></div>
-        <div className="border w-24 h-24 rounded-lg"></div>
+        <div className="border-2 border-green-500 w-24 h-24 rounded-lg"></div>
+        <div className="border-2 border-green-500 w-24 h-24 rounded-lg"></div>
+        <div className="border-2 border-green-500 w-24 h-24 rounded-lg"></div>
+        <div className="border-2 border-green-500 w-24 h-24 rounded-lg"></div>
+        <div className="border-2 border-green-500 w-24 h-24 rounded-lg"></div>
+        <div className="border-2 border-green-500 w-24 h-24 rounded-lg"></div>
       </div>
     </div>
   );
